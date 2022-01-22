a3b16ab508faec64a2ffce3cb69324cb77edf73c
vivekbigelow <vivekbigelow1234@gmail.com>
2022-01-21 19:38:17 -0500

    First template

--- src/library/views.py
+++ src/library/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Book
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello World!!")
+  books = Book.objects.order_by('title')
+  template = loader.get_template('library/index.html')
+  context = {
+    'books':books,
+  }
+
+  return HttpResponse(template.render(context, request))
+
+def bookDetail(request, book_id):
+  response = "You're looking at the details of book %s."
+  return HttpResponse(response % book_id)
+
