c96e937926ee1b2c32bce288afaf66f4949e4a9a
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-01-24 16:25:45 -0500

    Add a new book form

--- src/library/urls.py
+++ src/library/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'library'
 urlpatterns = [
   path('', views.index, name='index'),
-  path('<int:book_id>/', views.bookDetail, name='bookDetail')
+  path('<int:book_id>/', views.bookDetail, name='bookDetail'),
+  path('new-book-form/', views.newBookForm, name='newBookForm'),
+  path('process-new-book', views.processNewBook, name='processNewBook')
 ]
--- src/library/views.py
+++ src/library/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
-from .models import Book
+from .models import Book, Author, Genre
+
 
 # Create your views here.
 def index(request):
@@ -15,4 +18,29 @@ def index(request):
 def bookDetail(request, book_id):
   book = get_object_or_404(Book, pk=book_id)
   return render(request, 'library/book-detail.html', {'book': book})
+
+def newBookForm(request):
+  authors = Author.objects.all()
+  genres = Genre.objects.all()
+
+  return render(request, 'library/new-book-form.html', {'authors':authors, 'genres':genres})
+
+def processNewBook(request):
+  title = request.POST['title']
+  author_id = request.POST['author']
+  genre_id = request.POST['genre']
+
+  if title and author_id and genre_id:
+    author = get_object_or_404(Author, pk=author_id)
+    genre = get_object_or_404(Genre, pk=genre_id)
+    book = Book(title=title, author=author, genre=genre)
+    book.save()
+    return HttpResponseRedirect(reverse('library:index'))
+  else:
+    authors = Author.objects.all()
+    genres = Genre.objects.all()
+    return render(request, 'library/new-book-form.html', {'authors':authors, 'genres':genres, 'error_message': "Incorrect Book Data"})
+
+  
+
 
