8d1d9fbb593f0da6c5fd05829065183d372aad87
Vivek Bigelow <vivek.bigelow@iga.in.gov>
2022-01-25 11:50:48 -0500

    Tutorial part 4, author detail, genre detail

--- src/library/urls.py
+++ src/library/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'library'
 urlpatterns = [
   path('', views.index, name='index'),
-  path('<int:book_id>/', views.bookDetail, name='bookDetail'),
+  path('<int:pk>/', views.BookDetailView.as_view(), name='bookDetail'),
+  path('author/<int:author_id>/', views.authorDetail, name='authorDetail'),
+  path('genre/<int:genre_id>', views.genreDetail,name='genreDetail'),
   path('new-book-form/', views.newBookForm, name='newBookForm'),
-  path('process-new-book', views.processNewBook, name='processNewBook')
+  path('process-new-book', views.processNewBook, name='processNewBook'),
 ]
--- src/library/views.py
+++ src/library/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 
 from .models import Book, Author, Genre
@@ -15,9 +16,19 @@ def index(request):
 
   return render(request, 'library/index.html', context)
 
-def bookDetail(request, book_id):
-  book = get_object_or_404(Book, pk=book_id)
-  return render(request, 'library/book-detail.html', {'book': book})
+class BookDetailView(generic.DetailView):
+  model = Book
+  template_name = 'library/book-detail.html'
+
+def authorDetail(request, author_id):
+  author = get_object_or_404(Author, pk=author_id)
+  books = Book.objects.filter(author_id=author_id)
+  return render(request, 'library/author-detail.html', {"author":author, "books":books})
+
+def genreDetail(request, genre_id):
+  genre = get_object_or_404(Genre, pk=genre_id)
+  books = Book.objects.filter(genre_id=genre_id)
+  return render(request, 'library/genre-detail.html', {"genre":genre, "books":books})
 
 def newBookForm(request):
   authors = Author.objects.all()
@@ -35,11 +46,11 @@ def processNewBook(request):
     genre = get_object_or_404(Genre, pk=genre_id)
     book = Book(title=title, author=author, genre=genre)
     book.save()
-    return HttpResponseRedirect(reverse('library:index'))
+    return HttpResponseRedirect(reverse('library:bookDetail', args=(book.pk,)))
   else:
     authors = Author.objects.all()
     genres = Genre.objects.all()
-    return render(request, 'library/new-book-form.html', {'authors':authors, 'genres':genres, 'error_message': "Incorrect Book Data"})
+    return render(request, 'library/new-book-form.html', {'authors':authors, 'genres':genres, 'error_message': "Improper Book Data"})  
 
   
 
